416070405ff2accb800513d624375654ac269eaa
numirias <numirias@users.noreply.github.com>
2018-02-12 14:17:18 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python Semantic Syntax Highlighting
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='python-semantic-syntax',
+    description='Real semantic syntax highlighting for Python',
+    packages=['sem'],
+    author='numirias',
+    author_email='numirias@users.noreply.github.com',
+    version='0.1.0',
+    url='TODO',
+    license='MIT',
+    python_requires='>=3',
+    install_requires=[
+        'pytest>=3.3.2',
+        'pytest-metadata',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Editors',
+    ],
+)
